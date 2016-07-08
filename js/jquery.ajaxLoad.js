246db4ea36f5dc5ad1c85bd5527840633691cc7a
pirog.v <pirog.v@gmail.com>
2016-07-08 10:56:57 +0300

    css

---
+++ js/jquery.ajaxLoad.js
@@ -0,0 +1,51 @@
+(function( $ ) {
+	$.fn.ajaxLoad = function(options)
+	{
+		settings = $.extend(
+		{
+			effect		: 'insert',
+			form		: '',
+			href		: '/',
+			type		: 'POST',
+			cache		: false,
+			datatype	: 'html'
+		}, options);
+		
+		box = this;
+
+		$.ajax({
+			url			: settings.href,
+			dataType	: settings.datatype,
+			data		: getDataForm(settings.form),
+			cache		: settings.cache,
+			type		: settings.type,
+			beforeSend	: function()
+			{
+
+			},
+			success		: function(data)
+			{
+				if(settings.effect == 'insert') insert (data);
+			}
+		});
+
+		function insert (data)
+		{
+			$(box).html(data);
+		}
+
+		function getDataForm(data)
+		{
+			if(data != '') {
+				if(data.indexOf('=') == -1) {
+					return $('form[name="'+options.form+'"]').serialize();
+				} else {
+					return data;
+				}
+			}
+			return '';
+		}
+
+		return this;
+	}
+})(jQuery);
