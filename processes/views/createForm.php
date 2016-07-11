6269c1bd21d4e21a8d43e1287ff9fd6dd3263443
pirog.v <pirog.v@gmail.com>
2016-07-11 08:25:30 +0300

    Создание карточки

---
+++ processes/views/createForm.php
@@ -0,0 +1,21 @@
+<?php 
+
+use common\Form;
+
+?>
+<div id="box-card">
+<div class="P layout">
+<div><a href="/">Назад</a></div>
+	<?=Form::begin($model->rules(), ['name' => 'card', 'head' => '', 'method' => 'post'])?>
+		<div class="col1">
+			<?=Form::text50('series', 'Серия')?>
+			<?=Form::radio('expiration_date', $model->expiration_date, 'Срок действия')?>
+			<?=Form::text50('number', 'Количество')?>
+		</div>
+		<div class="mar3">
+			<?=Form::submit()?>
+		</div>
+		<div class="form-wrap-input"><div class="LN"></div></div>
+	<?=Form::end()?>
+</div>
+</div>
